b612622bf777881d941b82b19c035aa851ad6123
aidadieudonne <aidaharing10@gmail.com>
2025-04-30 18:07:13 -0600

    studying a match method

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = []
+        
+        for candidate in word_list:
+            if sorted(candidate) == sorted_word:
+                matches.append(candidate)
+
+        return matches
